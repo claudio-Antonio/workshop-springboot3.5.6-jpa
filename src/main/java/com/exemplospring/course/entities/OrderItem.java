5d6308dc9146755047adbf7b544b2200ddffcf07
Cláudio Antônio <claudioanery@gmail.com>
2025-10-20 17:15:52 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/exemplospring/course/entities/OrderItem.java
+++ src/main/java/com/exemplospring/course/entities/OrderItem.java
@@ -29,7 +29,6 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
-    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
@@ -38,6 +37,7 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
+    @JsonIgnore
     public Product getProduct() {
         return id.getProduct();
     }
